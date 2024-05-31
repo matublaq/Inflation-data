69468f2ca82e64dcc333ee311d2677671a3cc3c4
Matublaq <mati.blaquier@gmail.com>
2024-05-31 20:43:21 +0100

    none

--- InflationDB.py
+++ InflationDB.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql
 
+
 def InflationDB_scheme():
     conn = sql.connect('InflationDB.db') # Connect or create if that not exist
     cursor = conn.cursor()
@@ -36,5 +37,28 @@ def InflationDB_scheme():
 ''')
     
     # Save changes and close connection
+    conn.commit()
+    conn.close()
+
+
+def insert_data_into_inflationDB(df):
+    # Data-base connect
+    conn = sql.connect('InflacionDB.db')
+    cursor = conn.cursor()
+    cursor.execute('PRAGMA foreign_keys = ON') # Activate foreign keys
+
+
+    # Insert countries with your codes
+    countries = df[['country_code', 'country_name']].drop_duplicates().values.tolist()
+    cursor.executemany('INSERT OR IGNORE countries (country_code, country_name) VALUES (?, ?)', countries)
+
+    # Insert years
+    years = df['year'] # years columns
+    cursor.executemany('INSERT OR IGNORE INTO years (year) VALUES (?)', years)
+
+    # Transform al load inflation rates
+    inflation_data = df[['country_code', 'year', 'inflation_rate']].values.tolist()
+    cursor.executemany('INSERT OR IGNORE INTO inflation_data (country_code, year, inflation_rate) VALUES (?, ?, ?)', inflation_data)
+
     conn.commit()
     conn.close()
